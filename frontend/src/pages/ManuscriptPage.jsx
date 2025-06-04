5c8eb795d3da6224086a211caf2d90e3c9ffcdc4
Harsahibjit Singh <harsahibjitsingh@icloud.com>
2025-06-04 23:08:41 +0530

    Redirection link changed for mysubmission in manuscript page

--- frontend/src/pages/ManuscriptPage.jsx
+++ frontend/src/pages/ManuscriptPage.jsx
@@ -1848,7 +1848,7 @@ const ManuscriptPage = () => {
 									<button
 										type="button"
 										onClick={() =>
-											navigate("/my-submissions")
+											navigate("/journal/Journal-of-Intelligent-Computing-Systems/my-submissionss")
 										}
 										className="mt-4 px-6 py-2 bg-[#496580] text-white rounded-lg hover:bg-[#3a5269]"
 									>
